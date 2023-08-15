f592394855afce4ef05ae6112d9d39db808f2323
Abdelrahman Yosry <87338453+AbdelrahmanYosry1507@users.noreply.github.com>
2023-08-15 13:12:43 +0300

    enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//component directive
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angulr1';
+  var1:boolean = true
+
+//  name='Ahmed'
+//  title="training"
+//  img='https://picsum.photos/200/300'
+//  binding="HEELOO"
+links=['Home',"About", 'Contact Us']
+ submit(){
+  console.log("hi ")
+  this.var1 =false
+ }
+name1:string ="Ali"
+name2='Khloud'
+
 }
